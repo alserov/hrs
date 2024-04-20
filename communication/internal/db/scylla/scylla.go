ecd6acbf99a6d2758a8e2a3ecf00514c044df287
Alex Rokachev <alserokachev@gmail.com>
2024-04-20 11:37:12 +0300

    started db layer

--- communication/internal/db/scylla/scylla.go
+++ communication/internal/db/scylla/scylla.go
@@ -4,32 +4,54 @@ import (
 	"context"
 	"github.com/alserov/hrs/communication/internal/db"
 	"github.com/alserov/hrs/communication/internal/usecase/models"
+	"github.com/gocql/gocql"
+	"github.com/scylladb/gocqlx/v2"
 )
 
 var _ db.Repository = &Scylla{}
 
-func NewRepository() *Scylla {
-	return &Scylla{}
+func NewRepository(s gocqlx.Session) *Scylla {
+	return &Scylla{s}
 }
 
-func Connect() {
+func MustConnect(dsn string) gocqlx.Session {
+	cluster := gocql.NewCluster(dsn)
 
+	ws, err := gocqlx.WrapSession(cluster.CreateSession())
+	if err != nil {
+		panic("failed to create session: " + err.Error())
+	}
+
+	return ws
 }
 
 type Scylla struct {
+	gocqlx.Session
 }
 
 func (s Scylla) CreateMessage(ctx context.Context, msg models.Message) error {
-	//TODO implement me
-	panic("implement me")
+
+	//if err != nil {
+	//	return utils.NewError(utils.ErrInternal, err.Error())
+	//}
+
+	return nil
 }
 
 func (s Scylla) DeleteMessage(ctx context.Context, msgID string) error {
-	//TODO implement me
-	panic("implement me")
+
+	//if err != nil {
+	//	return utils.NewError(utils.ErrInternal, err.Error())
+	//}
+
+	return nil
 }
 
 func (s Scylla) EditMessage(ctx context.Context, edit models.MessageEdit) error {
-	//TODO implement me
-	panic("implement me")
+
+	//if err != nil {
+	//	return utils.NewError(utils.ErrInternal, err.Error())
+	//}
+
+	return nil
 }
--- communication/internal/usecase/models/models.go
+++ communication/internal/usecase/models/models.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Message struct {
-	ID          string    `json:"id"`
-	Value       string    `json:"value"`
-	Files       []string  `json:"files"`
-	SenderID    string    `json:"senderID"`
-	RecipientID string    `json:"recipientID"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	ID          string    `json:"id" db:"id"`
+	Value       string    `json:"value" db:"value"`
+	Files       []string  `json:"files" db:"-"`
+	SenderID    string    `json:"senderID" db:"sender_id"`
+	RecipientID string    `json:"recipientID" db:"recipient_id"`
+	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt   time.Time `json:"updatedAt" db:"updated_at"`
 }
 
 type MessageEdit struct {
